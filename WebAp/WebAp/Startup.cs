03dc189ceac534ddc43e24384fab9854c49256b8
valexan <avarfolo@gmail.com>
2017-01-19 09:40:37 -0800

    Changes

---
+++ WebAp/WebAp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAp.Startup))]
+namespace WebAp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
